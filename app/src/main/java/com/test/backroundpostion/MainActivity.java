21fec0524108e35a84dc305cc6380eacd4167275
JaL3OuT <mjihedbh@gmailcom>
2019-02-18 18:48:13 +0100

    rest of code

--- app/src/main/java/com/test/backroundpostion/MainActivity.java
+++ app/src/main/java/com/test/backroundpostion/MainActivity.java
@@ -1,49 +1,57 @@
 package com.test.backroundpostion;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.test.backroundpostion.Service.TrackerService;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private static final int PERMISSIONS_REQUEST = 1;
-    Button stop ;
+    Button stop;
+    TextView lati, lang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        stop= findViewById(R.id.stopservice);
+        initview();
         chekGpsPermisson();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter("GPSLocationUpdates"));
     }
-  public  void chekGpsPermisson (){
-      // Check GPS is enabled
-      LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
-      if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-          Toast.makeText(this, "Please enable location services", Toast.LENGTH_SHORT).show();
-          finish();
-      }
-      // Check location permission is granted - if it is, start
-      // the service, otherwise request the permission
-      int permission = ContextCompat.checkSelfPermission(this,
-              Manifest.permission.ACCESS_FINE_LOCATION);
-      if (permission == PackageManager.PERMISSION_GRANTED) {
-          startTrackerService();
-      } else {
-          ActivityCompat.requestPermissions(this,
-                  new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                  PERMISSIONS_REQUEST);
-      }
+
+    public void chekGpsPermisson() {
+        // Check GPS is enabled
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(this, "Please enable location services", Toast.LENGTH_SHORT).show();
+        }
+        // Check location permission is granted - if it is, start
+        // the service, otherwise request the permission
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            startTrackerService();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST);
+        }
     }
+
     private void startTrackerService() {
         startService(new Intent(this, TrackerService.class));
     }
@@ -56,11 +64,28 @@ public class MainActivity extends AppCompatActivity {
             // Start the service when the permission is granted
             startTrackerService();
         } else {
-            finish();
+
         }
     }
 
-    public void stopserviceBackground(View view) {
-        stopService(new Intent(MainActivity.this , TrackerService.class));
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Get extra data included in the Intent
+            String message = intent.getStringExtra("Status");
+            Bundle b = intent.getBundleExtra("Location");
+            String lat = b.getString("lat");
+            String lng = b.getString("lng");
+            lati.setText(lat);
+            lang.setText(lng);
+            Log.e("TAG", "onReceive: " + lng + "/" + lat);
+        }
+    };
+    public void initview() {
+        stop = findViewById(R.id.stopservice);
+        lati = findViewById(R.id.latV);
+        lang = findViewById(R.id.longV);
     }
+
 }
--- app/src/main/java/com/test/backroundpostion/Service/TrackerService.java
+++ app/src/main/java/com/test/backroundpostion/Service/TrackerService.java
@@ -14,9 +14,10 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -26,44 +27,28 @@ import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.test.backroundpostion.R;
 
-import org.w3c.dom.Text;
-
 import static android.support.v4.app.NotificationCompat.Builder;
 
 public class TrackerService extends Service {
     private static final String TAG = TrackerService.class.getSimpleName();
     Location location;
-    String tText ="" ;
+    String tText = "";
 
     public TrackerService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
+
         return null;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        requestLocationUpdates();
         startMyOwnForeground(tText);
+        requestLocationUpdates();
 
-
-    }
-
-    private void buildNotification() {
-        String stop = "stop";
-        registerReceiver(stopReceiver, new IntentFilter(stop));
-        PendingIntent broadcastIntent = PendingIntent.getBroadcast(
-                this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
-        Builder builder = new Builder(this)
-                .setContentTitle(getString(R.string.app_name))
-                .setContentText(getString(R.string.notifixcation_text))
-                .setOngoing(true)
-                .setContentIntent(broadcastIntent)
-                .setSmallIcon(R.drawable.ic_tracker);
-        startForeground(1, builder.build());
     }
 
     protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
@@ -73,12 +58,10 @@ public class TrackerService extends Service {
             // Stop the service when the notification is tapped
             unregisterReceiver(stopReceiver);
             stopSelf();
-
         }
     };
 
-    private void requestLocationUpdates() {
-
+    public void requestLocationUpdates() {
         LocationRequest request = new LocationRequest();
         request.setInterval(10000);
         request.setFastestInterval(5000);
@@ -87,11 +70,11 @@ public class TrackerService extends Service {
         int permission = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION);
         if (permission == PackageManager.PERMISSION_GRANTED) {
-
             client.requestLocationUpdates(request, new LocationCallback() {
                 @Override
                 public void onLocationResult(LocationResult locationResult) {
                     location = locationResult.getLastLocation();
+                    Log.d(TAG, "Im here " + location);
                     if (location != null) {
                         updateNotification();
                         Log.d(TAG, "location update " + location);
@@ -102,33 +85,38 @@ public class TrackerService extends Service {
     }
 
     private Notification startMyOwnForeground(String text) {
-
         String stop = "stop";
         registerReceiver(stopReceiver, new IntentFilter(stop));
         PendingIntent broadcastIntent = PendingIntent.getBroadcast(
                 this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
-        //for the new version ANdroid 8.1 & 9
+        //for the new version Android 8.1 & 9
         // // New channel
-        String NOTIFICATION_CHANNEL_ID = "com.test.backroundpostion.Service";
+        String NOTIFICATION_CHANNEL_ID = "background";
         String channelName = "My Background Service";
         NotificationChannel chan = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-        {
-            chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
-
-            chan.setLightColor(Color.BLUE);
-
-            chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_LOW);
         }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            chan.setLightColor(Color.BLUE);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        }
+
+
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         assert manager != null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            manager.createNotificationChannel(chan);
+        }
 
         Builder notificationBuilder = new Builder(this, NOTIFICATION_CHANNEL_ID);
         Notification notification = notificationBuilder.setOngoing(true)
                 .setSmallIcon(R.drawable.ic_tracker)
                 .setContentTitle(getString(R.string.notifixcation_text))
-                .setPriority(NotificationManager.IMPORTANCE_MIN)
                 .setCategory(Notification.CATEGORY_SERVICE)
                 .setContentText(text)
                 .setContentIntent(broadcastIntent)
@@ -138,14 +126,26 @@ public class TrackerService extends Service {
 
         return notification;
     }
-  private void updateNotification() {
-        String text ,mLat ,mLang ;
+
+    private void updateNotification() {
+        String text, mLat, mLang;
         mLat = String.valueOf(location.getLatitude());
-        mLang = String.valueOf(location.getLongitude()) ;
-        text = mLat + mLang ;
-      Notification notification = startMyOwnForeground(text);
+        mLang = String.valueOf(location.getLongitude());
+        text = mLat + mLang;
+        sendMessageToActivity(mLat, mLang);
+        Notification notification = startMyOwnForeground(text);
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.notify(2, notification);
     }
+
+    private void sendMessageToActivity(String lat, String lng) {
+        Intent intent = new Intent("GPSLocationUpdates");
+        Bundle b = new Bundle();
+        b.putString("lat", lat);
+        b.putString("lng", lng);
+        intent.putExtra("Location", b);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
 }
 
